4ecd1f1825f83a01a6188ddc37a81a56ba44cf1c
Sümeyye Zülal Dik <115083072+szulaldik@users.noreply.github.com>
2023-10-12 15:42:13 +0300

    Update MainWindow_DL_Controller.py
    PDF view is fixed

--- Windows/MainWindow_DL_Controller.py
+++ Windows/MainWindow_DL_Controller.py
@@ -1,10 +1,5 @@
-import subprocess
-
-import fitz
-from PySide6.QtGui import QImage, Qt, QPixmap
+from PySide6.QtCore import QUrl
 from PySide6.QtWidgets import QMessageBox, QMainWindow
-
-
 from System import ImageProcessing
 from System.ImageOperation import ImageOperation
 from Windows.MainWindow_DL_UI import Ui_MainWindow_DL
@@ -91,7 +86,7 @@ class MainWindow_DL_Controller():
         self.ui.gv_image.scene.Update_Branch_Paths(isVisible, showId, showLenght)
 
     def pbtn_menu_report_clicked(self):
-        self.ui.wgts_sceneContent.setCurrentIndex(2)
+        self.ui.wgts_sceneContent.setCurrentWidget(self.ui.page_select_report_options)
         return
 
     def pbtn_create_pdf_clicked(self):
@@ -119,10 +114,8 @@ class MainWindow_DL_Controller():
         informationDict["p2_type"] = veinStartEndPointsType
 
         file_path = ImageOperation.SaveInfos(self.ui.gv_image.scene.vap_image, informationDict)
-
-        subprocess.Popen([file_path], shell=True)
         self.ui.wgts_sceneContent.setCurrentWidget(self.ui.page_report)
-        self.load_pdf(file_path)
+        self.show_pdf(file_path)
         return
 
     def pbtn_menu_close_clicked(self):
@@ -132,41 +125,10 @@ class MainWindow_DL_Controller():
             self.mainWindow.close()
         return
 
-    def load_pdf(self,file_name):
-        if file_name:
-            self.pdf_document = fitz.open(file_name)
-            self.show_page()
-
-    def show_page(self):
-        if not self.pdf_document:
-            return
-
-        page = self.pdf_document.load_page(self.page_index)
-        image = page.get_pixmap()
-        qt_image = QImage(image.samples, image.width, image.height, image.stride, QImage.Format_RGB888)
-        pixmap = QPixmap.fromImage(qt_image)
-
-        # Calculate the scale factor to fit the page to the view
-        scale_factor = min(self.ui.pdf_view.width() / pixmap.width(), self.ui.pdf_view.height() / pixmap.height()) * 1.5
-        scaled_pixmap = pixmap.scaled(pixmap.width() * scale_factor, pixmap.height() * scale_factor, Qt.KeepAspectRatio)
-
-        # Calculate the center position to display the page in the view
-        center_x = (self.ui.pdf_view.width() - scaled_pixmap.width()) / 2
-        center_y = (self.ui.pdf_view.height() - scaled_pixmap.height()) / 2
-
-        top_left_x = center_x if center_x > 0 else 0
-        top_left_y = center_y if center_y > 0 else 0
-
-        self.ui.scene.clear()
-        self.ui.scene.addPixmap(scaled_pixmap)
-        self.ui.pdf_view.setSceneRect(top_left_x, top_left_y, scaled_pixmap.width(), scaled_pixmap.height())
-
-    def previous_page(self):
-        if self.page_index > 0:
-            self.page_index -= 1
-            self.show_page()
-
-    def next_page(self):
-        if self.pdf_document and self.page_index < len(self.pdf_document) - 1:
-            self.page_index += 1
-            self.show_page()
+        def show_pdf(self,file_path):
+        self.ui.webEngine.settings().setAttribute(self.ui.webEngine.settings().WebAttribute.PluginsEnabled,
+                                                      True)
+        self.ui.webEngine.settings().setAttribute(self.ui.webEngine.settings().WebAttribute.PdfViewerEnabled,
+                                                      True)
+        self.ui.webEngine.setUrl(QUrl("file:///" + file_path.replace('\\', '/')))
+        return
